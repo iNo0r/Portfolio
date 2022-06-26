0f99966750309162d1d575fe92b451cde0c27c2e
King <pic.ner101@gmail.com>
2022-06-26 16:35:37 +0100

    adding new project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import store from './store'
+
+AOS.init()
 
 createApp(App).use(store).use(router).mount('#app')
